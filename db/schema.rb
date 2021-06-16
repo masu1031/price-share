5fb5520cbb8f7144a63749b534f9ce3832611bbb
masu1031 <hibiki.masumoto@gmail.com>
2021-06-16 15:13:07 +0900

    エラー修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_08_074634) do
+ActiveRecord::Schema.define(version: 2021_05_08_074634) do
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", default: "", null: false
