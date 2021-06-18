01de1d4f469b2bee8c163548f4c5deb4978a88c5
masu1031 <hibiki.masumoto@gmail.com>
2021-06-18 16:15:09 +0900

    Revert "README＋その他の修正"
    This reverts commit bca978834390d7fd3219538874e2061404fa9282.

--- db/migrate/20210207080432_create_cards.rb
+++ db/migrate/20210207080432_create_cards.rb
@@ -3,9 +3,9 @@ class CreateCards < ActiveRecord::Migration[6.0]
     create_table :cards do |t|
       t.string     :name,         null: false, default: ""
       t.integer    :expansion_id, null: false
-      t.integer    :rarity_id
+      t.integer    :rarity_id,    null: false
       t.integer    :card_type_id, null: false
-      t.integer    :attribute_id
+      t.integer    :attribute_id, null: false
       t.references :group,        null: false, foreign_key: true
 
       t.timestamps
