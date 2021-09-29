5d798f1442556c6fb6d904733216ae02b4a9be11
masu1031 <hibiki.masumoto@gmail.com>
2021-09-30 08:20:13 +0900

    新団追加アプデ

--- app/models/attribute.rb
+++ app/models/attribute.rb
@@ -2,7 +2,8 @@ class Attribute < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
     { id: 2, name: '一撃' },
-    { id: 3, name: '連撃' }
+    { id: 3, name: '連撃' },
+    { id: 4, name: 'フュージョン' }    
   ]
 
   include ActiveHash::Associations
